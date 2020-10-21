c23efb886d0af5b80fa8fa19d7bdd86a83f24875
idevmans <man1080@yandex.ru>
2020-10-21 18:56:38 +0400

    Парсер сайта https://yarcube.ru/

---
+++ components/parser/news/YarCubeParser.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace app\components\parser\news;
+
+use app\components\helper\nai4rus\AbstractBaseParser;
+use app\components\helper\nai4rus\PreviewNewsDTO;
+use app\components\parser\NewsPost;
+use DateTimeImmutable;
+use DateTimeZone;
+use RuntimeException;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\DomCrawler\UriResolver;
+use Throwable;
+
+class YarCubeParser extends AbstractBaseParser
+{
+    public const USER_ID = 2;
+    public const FEED_ID = 2;
+
+    protected function getSiteUrl(): string
+    {
+        return 'https://yarcube.ru';
+    }
+
+    protected function getPreviewNewsDTOList(int $minNewsCount = 10, int $maxNewsCount = 100): array
+    {
+        $previewNewsDTOList = [];
+        $pageNumber = 1;
+
+        while (count($previewNewsDTOList) < $maxNewsCount) {
+            if ($pageNumber > 1) {
+                $urn = "/newsletter/?PAGEN_1={$pageNumber}";
+            } else {
+                $urn = "/newsletter/";
+            }
+            $uriPreviewPage = UriResolver::resolve($urn, $this->getSiteUrl());
+            $pageNumber++;
+
+            try {
+                $previewNewsContent = $this->getPageContent($uriPreviewPage);
+                $previewNewsCrawler = new Crawler($previewNewsContent);
+            } catch (Throwable $exception) {
+                if (count($previewNewsDTOList) < $minNewsCount) {
+                    throw new RuntimeException('Не удалось получить достаточное кол-во новостей', null, $exception);
+                }
+                break;
+            }
+
+            $previewNewsXPath = '//section[@class="site-section _feed-section"]//div[@class="feed-news__inner-wrp feed-news__flex-row"]';
+            $previewNewsCrawler = $previewNewsCrawler->filterXPath($previewNewsXPath);
+
+            $previewNewsCrawler->each(function (Crawler $newsPreview) use (&$previewNewsDTOList) {
+                $titleCrawler = $newsPreview->filterXPath('//a[@class="feed-news__title"]');
+                $title = $titleCrawler->text();
+                $uri = UriResolver::resolve($titleCrawler->attr('href'), $this->getSiteUrl());
+
+                $publishedAtString = $newsPreview->filterXPath('//div[contains(@class,"feed-news__date")]')->text();
+                $publishedAtString = explode(' ', $publishedAtString);
+                unset($publishedAtString[3]);
+                $this->convertStringMonthToNumber($publishedAtString[1]);
+                $publishedAtString[1] = $this->convertStringMonthToNumber($publishedAtString[1]);
+                //[1] - day // [2] - month // [3] - year
+                $publishedAtString = $publishedAtString[0].' '.$publishedAtString[1].' '.$publishedAtString[2];
+                $timezone = new DateTimeZone('Europe/Moscow');
+                $publishedAt = DateTimeImmutable::createFromFormat('d m Y', $publishedAtString, $timezone);
+                $publishedAtUTC = $publishedAt->setTimezone(new DateTimeZone('UTC'));
+
+                $description = null;
+                $previewNewsDTOList[] = new PreviewNewsDTO($this->encodeUri($uri), $publishedAtUTC, $title, $description);
+            });
+        }
+
+        $previewNewsDTOList = array_slice($previewNewsDTOList, 0, $maxNewsCount);
+        return $previewNewsDTOList;
+    }
+
+    protected function parseNewsPage(PreviewNewsDTO $previewNewsDTO): NewsPost
+    {
+        $uri = $previewNewsDTO->getUri();
+        $image = null;
+
+        $newsPage = $this->getPageContent($uri);
+
+        $newsPageCrawler = new Crawler($newsPage);
+        $newsPostCrawler = $newsPageCrawler->filterXPath('//div[@class="news-detail__inner"]');
+
+        $mainImageCrawler = $newsPostCrawler->filterXPath('//img')->first();
+        if ($this->crawlerHasNodes($mainImageCrawler)) {
+            $image = $mainImageCrawler->attr('src');
+            $this->removeDomNodes($newsPostCrawler,'//img[1]');
+        }
+        if ($image !== null && $image !== '') {
+            $image = UriResolver::resolve($image, $uri);
+            $previewNewsDTO->setImage($this->encodeUri($image));
+        }
+
+        $previewNewsDTO->setDescription(null);
+
+        $contentCrawler = $newsPostCrawler->filterXPath('//div[@itemprop="description"]');
+
+        $this->purifyNewsPostContent($contentCrawler);
+
+        $newsPostItemDTOList = $this->parseNewsPostContent($contentCrawler, $previewNewsDTO);
+
+        return $this->factoryNewsPost($previewNewsDTO, $newsPostItemDTOList);
+    }
+
+    public function convertStringMonthToNumber($stringMonth): int
+    {
+        $stringMonth = mb_strtolower($stringMonth);
+        $monthsList = [
+            "января" => 1,
+            "февраля" => 2,
+            "марта" => 3,
+            "апреля" => 4,
+            "мая" => 5,
+            "июня" => 6,
+            "июля" => 7,
+            "августа" => 8,
+            "сентября" => 9,
+            "октября" => 10,
+            "ноября" => 11,
+            "декабря" => 12,
+        ];
+        return $monthsList[$stringMonth];
+    }
+
+} 
